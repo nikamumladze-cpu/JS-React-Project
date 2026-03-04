8d688702056c8cecef1700a904cc33f8d7a3fcf7
nikamumladze-cpu <nika.mumladze@geolab.edu.ge>
2026-03-04 19:57:25 +0400

    JS-React-Project

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,22 @@
+import { defineConfig } from "vite"; 
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-  base: process.env.VERCEL ? "/" : "/JS-React-Project/",
+  base: "/",
 
   plugins: [react(), tailwindcss()],
+
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
 });
